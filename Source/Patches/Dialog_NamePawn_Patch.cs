7af94f1c1195cf6bb8e1ef876bd145681bcbef98
Justin <darkflame57@gmail.com>
2022-10-08 21:14:36 -0700

    Fully working in 1.4 beta

--- Source/Patches/Dialog_NamePawn_Patch.cs
+++ Source/Patches/Dialog_NamePawn_Patch.cs
@@ -26,7 +26,6 @@ namespace JobInBar
     {
         public static void Postfix(ref Pawn ___pawn, Rect inRect)
         {
-            //Log.Message("Running patch");
             Pawn pawn = ___pawn;
 
             Rect regionRect = inRect;
@@ -35,53 +34,31 @@ namespace JobInBar
             regionRect.height = 36f;
             regionRect.y += 90f;
 
-            float swatchSize = 24f;
-
-            PawnLabelCustomColors_WorldComponent labelsComp;
-            //labelsComp = Find.World.components.OfType<PawnLabelCustomColors_WorldComponent>().First();
-            labelsComp = PawnLabelCustomColors_WorldComponent.instance;
+            PawnLabelCustomColors_WorldComponent labelsComp = PawnLabelCustomColors_WorldComponent.instance;
             if (labelsComp == null)
             {
                 Log.Error("Could not find PawnLabelCustomColors_WorldComponent. Colors and show settings won't work.");
             }
 
-            //Color nameCol;
-            //Color jobCol;
-
             labelsComp.GetJobLabelColorFor(pawn, out Color jobCol);
 
-            Rect leftRect = new Rect(15f, 88f, inRect.width / 2f - 20f, 36f);
-            // Name recoloring disabled for now
-            /*
+            Rect rectToggle = new Rect(inRect.width - 112f, inRect.height - 90f, 64f, 36f);
+            Rect rectColorPicker = new Rect(inRect.width - 42f, inRect.height - 90f, 36f, 36f);
 
-            if (Widgets.ButtonText(nameRect, "JobInBar_NameColorButton".Translate(), true, true, true) || Widgets.ButtonInvisible(nameRect, true))
+            if (Widgets.ButtonInvisible(rectColorPicker, true))
             {
-                Find.WindowStack.Add(new Dialog_ColourPicker(nameCol,
-                (newColor) =>
-                {
-                    labelsComp.SetNameColorFor(pawn, newColor);
-                }
-                ));
+                Find.WindowStack.Add(
+                    new Dialog_ColourPicker(
+                        jobCol.ToOpaque(),
+                        (newColor) => labelsComp.SetJobLabelColorFor(pawn, newColor)
+                    )
+                );
             }
-            Rect swatchRectName = new Rect(nameRect.x+8f, nameRect.y+6f, swatchSize, swatchSize);
-            Widgets.DrawBoxSolid(swatchRectName, nameCol);*/
 
-            Rect rightRect = new Rect(inRect.width/2, 88f, inRect.width/2f - 20f, 36f);
-
-            if (Widgets.ButtonText(rightRect, "JobInBar_JobColorButton".Translate(), true, true, true) || Widgets.ButtonInvisible(rightRect, true))
-            {
-                Find.WindowStack.Add(new Dialog_ColourPicker(jobCol.ToOpaque(),
-                (newColor) =>
-                {
-                    labelsComp.SetJobLabelColorFor(pawn, newColor);
-                }
-                ));
-            }
-            Rect swatchRectJob = new Rect(rightRect.x+rightRect.width - swatchSize - 8f, rightRect.y+6f, swatchSize, swatchSize);
-            Widgets.DrawBoxSolid(swatchRectJob, jobCol);
+            Widgets.DrawBoxSolid(rectColorPicker, jobCol);
 
             bool shouldDraw = labelsComp.GetDrawJobLabelFor(pawn);
-            Widgets.CheckboxLabeled(leftRect, "JobInBar_ShowJobLabelFor".Translate(), ref shouldDraw, placeCheckboxNearText: true);
+            Widgets.CheckboxLabeled(rectToggle, "JobInBar_ShowJobLabelFor".Translate(), ref shouldDraw, placeCheckboxNearText: true);
             labelsComp.SetDrawJobLabelFor(pawn, shouldDraw);
         }
     }
