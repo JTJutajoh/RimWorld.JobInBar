37c77219d6063b4fc6d559eafbb67589247c4ba4
JT <jtjutajoh@gmail.com>
2025-06-25 16:32:06 -0700

    Compatibility patch for "veltaris.colonistbar" mod

--- Source/HarmonyPatches/Patch_ColonistBar_OnGUI_OffsetEquipped.cs
+++ Source/HarmonyPatches/Patch_ColonistBar_OnGUI_OffsetEquipped.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
 using HarmonyLib;
 using JetBrains.Annotations;
 using RimWorld;
+using UnityEngine;
 
 namespace JobInBar.HarmonyPatches;
 
@@ -22,15 +24,25 @@ internal static class Patch_ColonistBar_OnGUI_OffsetEquipped
     private static readonly MethodInfo? IsWeaponGetterAnchor = AccessTools.PropertyGetter(typeof(ThingDef), "IsWeapon");
     private static float OffsetPerLabel => 16f + Settings.ExtraOffsetPerLine;
 
+    static bool Prepare(MethodBase original)
+    {
+        if (!PatchManager.CheckForMod(Patch_ColonistBar_OnGUI_OffsetEquipped_ShowUtilityApparelCompat.TargetPackageId, out var modMetaData)) return true;
+
+        Log.Message(
+            $"\"{modMetaData!.Name}\" is active, skipping default {nameof(Patch_ColonistBar_OnGUI_OffsetEquipped)} patch...");
+        return false;
+    }
+
     [HarmonyPatch(typeof(ColonistBar), nameof(ColonistBar.ColonistBarOnGUI))]
     [HarmonyTranspiler]
     [UsedImplicitly]
     static IEnumerable<CodeInstruction> OffsetWeaponIcon(IEnumerable<CodeInstruction> instructions)
     {
+        var codes = new List<CodeInstruction>(instructions);
+
         if (IsWeaponGetterAnchor == null)
             throw new InvalidOperationException(
                 $"Couldn't find {nameof(IsWeaponGetterAnchor)} method for {nameof(Patch_ColonistBar_OnGUI_OffsetEquipped)}.{MethodBase.GetCurrentMethod()} patch");
-        var codes = new List<CodeInstruction>(instructions);
 
         for (var i = 0; i < codes.Count; i++)
         {
@@ -42,28 +54,8 @@ internal static class Patch_ColonistBar_OnGUI_OffsetEquipped
                 // Load the current entry's pawn
                 yield return new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(ColonistBar.Entry), "pawn")!);
                 // Load the offset from settings onto the stack
-                yield return CodeInstruction.CallClosure<Func<Pawn, float>>(pawn =>
-                {
-                    if (!Settings.ModEnabled) return 0f;
-                    if (!Settings.OffsetEquippedByLabels) return Settings.OffsetEquippedExtra;
-                    if (Settings.DrawLabelOnlyOnHover && LabelDrawer.HoveredPawn != pawn) return 0f;
-                    var offset = 0f;
-
-                    offset += Settings.JobLabelVerticalOffset;
-
-                    if (pawn.ShouldDrawJobLabel())
-                        offset += OffsetPerLabel;
-                    if (pawn.ShouldDrawIdeoLabel())
-                        offset += OffsetPerLabel;
-                    if (pawn.ShouldDrawRoyaltyLabel())
-                        offset += OffsetPerLabel;
-
-                    if (Settings.MoveWeaponBelowCurrentTask && LabelDrawer.HoveredPawn == pawn)
-                        offset += OffsetPerLabel;
-
-
-                    return offset + Settings.OffsetEquippedExtra;
-                })!;
+                yield return CodeInstruction.Call(typeof(Patch_ColonistBar_OnGUI_OffsetEquipped),
+                    nameof(GetOffsetFor))!;
                 // Add the offset to rect.y before it is used to construct a new Rect
                 yield return new CodeInstruction(OpCodes.Add);
             }
@@ -71,4 +63,96 @@ internal static class Patch_ColonistBar_OnGUI_OffsetEquipped
             yield return codes[i]!;
         }
     }
+
+    internal static float GetOffsetFor(Pawn pawn)
+    {
+        if (!Settings.ModEnabled) return 0f;
+        if (!Settings.OffsetEquippedByLabels) return Settings.OffsetEquippedExtra;
+        if (Settings.DrawLabelOnlyOnHover && LabelDrawer.HoveredPawn != pawn) return 0f;
+        var offset = 0f;
+
+        offset += Settings.JobLabelVerticalOffset;
+
+        if (pawn.ShouldDrawJobLabel())
+            offset += OffsetPerLabel;
+        if (pawn.ShouldDrawIdeoLabel())
+            offset += OffsetPerLabel;
+        if (pawn.ShouldDrawRoyaltyLabel())
+            offset += OffsetPerLabel;
+
+        if (Settings.MoveWeaponBelowCurrentTask && LabelDrawer.HoveredPawn == pawn)
+            offset += OffsetPerLabel;
+
+
+        return offset + Settings.OffsetEquippedExtra;
+    }
 }
+
+#region Mod Compatibility
+
+/// <summary>
+///     Compatibility patch for "[AV] Show Utility Apparel" mod:
+///     https://steamcommunity.com/sharedfiles/filedetails/?id=3266625851 <para />
+///     That mod's patch ignores the vanilla rect that the normal patch
+///     <see cref="Patch_ColonistBar_OnGUI_OffsetEquipped" />
+///     modifies in favor of its own static reference to a new rect.<br />
+///     This patch simply applies the same offset logic to that rect.
+/// </summary>
+[HarmonyPatch]
+[HarmonyPatchCategory("OffsetEquippedWeapon")]
+[SuppressMessage("ReSharper", "ArrangeTypeMemberModifiers")]
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+internal static class Patch_ColonistBar_OnGUI_OffsetEquipped_ShowUtilityApparelCompat
+{
+    internal const string TargetPackageId = "veltaris.colonistbar";
+
+    private static ModMetaData? ShowUtilityApparelMod;
+    private static List<Assembly>? ShowUtilityApparelAssemblies;
+
+    [UsedImplicitly]
+    static bool Prepare(MethodBase original)
+    {
+        if (!PatchManager.CheckForMod(TargetPackageId, out ShowUtilityApparelMod))
+        {
+            Log.Error(
+                $"Detected \"{TargetPackageId}\", but failed to get its metadata for compat patches.");
+            return false;
+        }
+
+        if (!PatchManager.TryGetModAssembly(ShowUtilityApparelMod?.PackageId ?? TargetPackageId, out ShowUtilityApparelAssemblies))
+        {
+            Log.Error(
+                $"Detected \"{ShowUtilityApparelMod?.Name ?? TargetPackageId}\" mod, but failed to find its assemblies for compat patches.");
+            return false;
+        }
+
+        Log.Message(
+            $"Doing alternate {nameof(Patch_ColonistBar_OnGUI_OffsetEquipped_ShowUtilityApparelCompat)} patch...");
+        return true;
+    }
+
+    [UsedImplicitly]
+    static IEnumerable<MethodBase> TargetMethods()
+    {
+        if (ShowUtilityApparelAssemblies is null)
+            yield break;
+
+        // Patch their patch
+        // Method is: AV_ColonistBar.ColonistBar_ColonistBarOnGUI_Patch.ColonistBar_ColonistBarOnGUI.After_Hook()
+        foreach (var type in ShowUtilityApparelAssemblies.Select(assembly => assembly.GetTypes()
+                     .FirstOrDefault(t => t.Name == "ColonistBar_ColonistBarOnGUI_Patch")!
+                     .GetNestedType("ColonistBar_ColonistBarOnGUI")!))
+            yield return AccessTools.Method(type, "After_Hook")!;
+    }
+
+    [HarmonyPrefix]
+    [UsedImplicitly]
+    static void OffsetEquipped(Pawn ___currentPawn, ref Rect ___currentRect)
+    {
+        var offset = Patch_ColonistBar_OnGUI_OffsetEquipped.GetOffsetFor(___currentPawn);
+
+        ___currentRect.y += offset;
+    }
+}
+
+#endregion Mod Compatibility
---
+++ Source/PatchManager.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using HarmonyLib;
+using JetBrains.Annotations;
+
+namespace JobInBar;
+
+/// <summary>
+///     Classes with this attribute are checked against the supplied condition(s) before patching
+/// </summary>
+[AttributeUsage(AttributeTargets.Class)]
+internal class HarmonyPatchLegacySupportAttribute : Attribute
+{
+    /// <summary>
+    ///     If this patch is skipped due to wrong RW version, this optional string will be included in the warning.
+    /// </summary>
+    internal readonly string? UnsupportedVersionString;
+
+    internal HarmonyPatchLegacySupportAttribute(
+        RWVersion supportedVersion = RWVersion.All,
+        RWVersion unsupportedVersion = RWVersion.None,
+        string? unsupportedVersionString = null
+    )
+    {
+        SupportedVersion = supportedVersion & ~unsupportedVersion;
+        UnsupportedVersionString = unsupportedVersionString;
+    }
+
+    internal RWVersion SupportedVersion { get; }
+
+    internal bool IsSupportedVersion => (LegacySupport.CurrentRWVersion & SupportedVersion) != 0;
+
+    internal bool ConditionResult => IsSupportedVersion;
+}
+
+/// <summary>
+///     Patch classes with this attribute will have Harmony.DEBUG enabled for patching
+/// </summary>
+internal class HarmonyDebugAttribute : Attribute
+{
+} //TODO: Implement this
+
+/// <summary>
+///     Helper class for all Harmony patching functionality.
+/// </summary>
+[StaticConstructorOnStartup]
+[UsedImplicitly]
+internal static class PatchManager
+{
+    internal static readonly Harmony Harmony;
+
+    private static int _loadedPatches;
+    private static int _failedPatches;
+    private static int _skippedPatches;
+
+    static PatchManager()
+    {
+        Harmony = new Harmony(JobInBarMod.Instance!.Content!.PackageId!);
+
+        Log.Message("Running Harmony patches...");
+
+        try
+        {
+            PatchAll();
+        }
+        catch (Exception e)
+        {
+            Log.Exception(e,
+                "Error doing Harmony patches. This likely means either the wrong game version or a hard incompatibility with another mod.");
+        }
+
+        var totalPatches = _loadedPatches + _failedPatches + _skippedPatches;
+        Log.Message($"{_loadedPatches}/{totalPatches} Harmony patches successful.");
+        if (_skippedPatches > 0)
+            Log.Message($"{_skippedPatches}/{totalPatches} Harmony patches skipped.");
+        if (_failedPatches > 0)
+            Log.Warning(
+                $"{_failedPatches}/{totalPatches} Harmony patches failed! The mod/game might behave in undesirable ways.");
+    }
+
+    internal static bool CheckForMod(string modIdentifier, out ModMetaData? otherMod)
+    {
+        otherMod = ModLister.GetActiveModWithIdentifier(modIdentifier);
+        return otherMod != null;
+    }
+
+    internal static bool TryGetModAssembly(string packageId, out List<Assembly>? assemblies)
+    {
+        var mod = LoadedModManager.RunningModsListForReading?.FirstOrDefault(m =>
+            m.PackageId!.ToLower() == packageId.ToLower());
+        assemblies = mod?.assemblies?.loadedAssemblies;
+        return assemblies != null;
+    }
+
+    internal static bool HasExistingPatches(this MethodInfo method, bool warn = true)
+    {
+        var patches = Harmony.GetPatchInfo(method);
+        if (patches is null) return false;
+
+        var hasPatches = patches.Prefixes?.Count > 0 || patches.Postfixes?.Count > 0 || patches.Transpilers?.Count > 0;
+
+        if (warn && hasPatches)
+        {
+            Log.Warning(
+                $"Found existing Harmony patches for {method.DeclaringType?.FullName}.{method.Name}. If you encounter compatibility issues, please report it on the Workshop page or GitHub issues.\nYou can safely ignore this warning if nothing seems broken.");
+            foreach (var patch in patches.Prefixes!.Union(patches.Postfixes!).Union(patches.Transpilers!))
+                Log.Message($"Patch: {patch.PatchMethod!.Module.Assembly.FullName}::{patch.PatchMethod.Name}");
+        }
+
+        return hasPatches;
+    }
+
+    private static void PatchAll()
+    {
+        PatchCategory("AddLabels");
+        PatchCategory("NamePawn");
+        PatchCategory("OffsetEquippedWeapon");
+        PatchCategory("PlaySettings");
+        PatchCategory("StopTracking");
+    }
+
+    /// <summary>
+    ///     Wrapper for <see cref="Harmony" />.<see cref="Harmony.PatchCategory(string)" /> that logs any errors that occur and
+    ///     skips patches that are disabled in the mod's configs.
+    /// </summary>
+    /// <param name="category">
+    ///     Name of the category to pass to <see cref="Harmony" />.
+    ///     <see cref="Harmony.PatchCategory(string)" />
+    /// </param>
+    private static void PatchCategory(string category)
+    {
+        var patchTypes = Assembly.GetExecutingAssembly()
+            .GetTypes()
+            .Where(t => t.GetCustomAttributes(typeof(HarmonyPatchCategory), true)
+                .Cast<HarmonyPatchCategory>()
+                .Any(attr => attr.info?.category == category))
+            .ToList();
+
+        // Find any classes in the assembly with a [HarmonyPatchCategory] attribute that matches the category
+        var numMethods = patchTypes.SelectMany(t =>
+                t.GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic))
+            .Count(m => m.GetCustomAttributes(typeof(HarmonyPatch), true).Length > 0);
+
+        // Find any [HarmonyPatchCondition] attributes on all the types in the category
+        var conditions = patchTypes
+            .SelectMany(t => t.GetCustomAttributes(typeof(HarmonyPatchLegacySupportAttribute), true)
+                .Cast<HarmonyPatchLegacySupportAttribute>())
+            .ToList();
+
+        // bitwise AND all of their supportedVersion
+        var supportedVersions = conditions.Aggregate(RWVersion.All,
+            (current, condition) => current & condition.SupportedVersion);
+
+        // If the result is not a supported version, fail
+        if ((supportedVersions & LegacySupport.CurrentRWVersion) == 0)
+        {
+            Log.Warning(
+                $"Patch category \"{category}\" ({numMethods} methods) skipped.\nOnly supported on RimWorld versions: {supportedVersions.ToString().Replace("_", ".").Replace("v", "")}.");
+            _skippedPatches += numMethods;
+
+            foreach (var condition in conditions)
+                if (condition.UnsupportedVersionString != null)
+                    Log.Message(condition.UnsupportedVersionString);
+
+            return;
+        }
+
+        try
+        {
+            Log.Trace($"Patching category \"{category}\" ({numMethods} methods)...");
+            Harmony.PatchCategory(category);
+        }
+        catch (Exception e)
+        {
+            Log.Exception(e, $"Error patching category {category}");
+            _failedPatches += numMethods;
+            return;
+        }
+
+        _loadedPatches += numMethods;
+    }
+}
